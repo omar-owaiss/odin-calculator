6544daf42ae44e40be22656433d9fd9fa8b87d03
omar owaiss <omarowaiss15@gmail.com>
2026-01-13 22:54:31 +0200

    Add a function that performs an operation according to the input it recieves

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ const multiply = function(num1, num2) {
 const divide = function(num1, num2) {
     return num1 / num2
 }
+
+const operate = function (num1, num2, operator) {
+    if (operator === "+") {
+        return add(num1, num2)
+    }
+    else if (operator === "-") {
+        return subtract(num1, num2)
+    }
+    else if (operator === "/") {
+        return divide(num1, num2)
+    }
+    else if (operator === "*") {
+        return multiply(num1, num2)
+    }
+    else {
+        return "please pick a valid number and operator"
+    }
+}
+
