bb7a822b0e69bb949f417c8b8e58353473798cd0
omar owaiss <omarowaiss15@gmail.com>
2026-01-14 23:31:53 +0200

    Add some logic to operate the calculator, Still needs a lot of fixes

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let firstNumber = ""
+let secondNumber = ""
+let operator = ""
+let isEnteringSecondNumber = false
+
+let btnDigit = document.querySelectorAll(".digit")
+let btnOperator = document.querySelectorAll(".operator")
+let btnClear = document.querySelector(".clear")
+let buttons = document.querySelector(".buttons")
+let display = document.querySelector(".numbers")
+let btnEqual = document.querySelector(".equal")
+
+
 const add = function(num1, num2) {
     return num1 + num2
 }
@@ -32,3 +45,35 @@ const operate = function (num1, num2, operator) {
     }
 }
 
+buttons.addEventListener("click", (e) => {
+    display.textContent = ""
+    if (e.target.classList.contains("digit")) {
+        if (!isEnteringSecondNumber) {
+        firstNumber += e.target.textContent
+        }
+        else {secondNumber += e.target.textContent}
+    }
+    else if (e.target.classList.contains("operator")) {
+        operator = e.target.textContent
+        isEnteringSecondNumber = true
+    }
+    display.textContent = display.textContent+`${firstNumber} ${operator} ${secondNumber}`
+})
+
+
+btnEqual.addEventListener("click", () => {
+    display.textContent = ""
+    let result = operate(firstNumber, secondNumber, operator)
+    display.textContent = `${result}`
+    firstNumber = result
+    secondNumber = ""
+    operator = ""
+    isEnteringSecondNumber = false
+})
+
+btnClear.addEventListener("click", () => {
+    firstNumber = ""
+    secondNumber = ""
+    operator = ""
+    isEnteringSecondNumber = false
+})
